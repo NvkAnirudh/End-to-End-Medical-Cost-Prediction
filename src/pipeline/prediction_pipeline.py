668c12f2c7ab50628b1bb8f72757246ad7b8d49a
NvkAnirudh <nuti.krish4@gmail.com>
2023-11-21 00:46:00 -0500

    Added prediction_pipeline.py

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -24,3 +24,42 @@ class PreditctPipeline:
         except Exception as e:
             raise CustomException(e, sys)
         
+# numerical_features = ['children']
+#             categorical_features = ['age_range', 'bmi_range', 'sex', 'smoker', 'region']
+class InputData:
+    def __init__(self,
+        age: int,
+        children: str,
+        bmi: float,
+        sex: str,
+        smoker: str,
+        region: str,
+        age_range: str,
+        bmi_range: str):
+
+        self.age = age
+        self.children = children
+        self.bmi = bmi
+        self.sex = sex
+        self.smoker = smoker
+        self.region = region
+        self.age_range = age_range
+        self.bmi_range = bmi_range
+
+    def get_data_as_dataFrame(self):
+        try:
+            input_dict = {
+                "age": [self.age],
+                "children": [self.children],
+                "bmi": [self.bmi],
+                "sex": [self.sex],
+                "smoker": [self.smoker],
+                "region": [self.region],
+                "age_range": [self.age_range],
+                "bmi_range": [self.bmi_range]
+            }
+
+            return pd.DataFrame(input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
